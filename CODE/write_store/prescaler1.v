87e1ee256c8db4c6f8387eb87f9664d1af9c0106
01FE19BEC251 <60061035+hemakongi@users.noreply.github.com>
2022-12-20 20:49:51 +0530

    Update prescaler1.v

--- CODE/write_store/prescaler1.v
+++ CODE/write_store/prescaler1.v
@@ -24,12 +24,9 @@ module prescaler1 (PCLK_k,PCLK);
 // Inputs
 
 input PCLK_k;
-//reg newclk=0;
 output reg PCLK=0;
-//output reg [31:0] count_n1=32'd0;
 
 reg [1:0] count_k=2'b0;
-//reg [8:0] count_n=9'b0;
 
 
 always @(posedge PCLK_k)
