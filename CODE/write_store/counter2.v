7229e2251c201b22df09fb7e9f0c1cc95ddf488c
01FE19BEC251 <60061035+hemakongi@users.noreply.github.com>
2022-12-20 20:50:43 +0530

    Update counter2.v

--- CODE/write_store/counter2.v
+++ CODE/write_store/counter2.v
@@ -27,21 +27,6 @@ output reg [7:0]store=0
 //output reg [7:0]out1=0
 );
 reg [7:0]out1=0;
-//reg [7:0]store;
-//parameter [7:0] out1=0;
-//reg [7:0] store=0;
-/*always @ (posedge newclk_k)
-begin
-if(c_enable==0)
-begin
-out1<=0;
-end
-else if(c_enable==1)
-begin
-out1<=out1+1'b1;
-end
-end
-endmodule*/
 
 
 always @ (posedge newclk_k)
